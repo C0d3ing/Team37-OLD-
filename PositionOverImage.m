7f9ef8c46955a453c56d076cfc6811c1c88df1c8
averya2 <avanderson@g.hmc.edu>
2024-03-20 21:42:51 -0700

    Image + Path Overlay Matlab
    Image and path overlay beginner code

---
+++ PositionOverImage.m
@@ -0,0 +1,14 @@
+t = [1: length(acccelY)]*0.099;
+yaw_error = yaw-yaw_des;
+
+map = imread('image.png'); %REPLACE WITH IMAGE NAME 
+map2 = flipud(map);
+imshow(map2);
+
+hold on 
+plot(x,y,'-')
+xlabel("X Position [m]")
+ylabel("Y Position [m]")
+title("Path Overlayed on Image")
+grid on;
+axis equal;
